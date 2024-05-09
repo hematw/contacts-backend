3394b44edc55351241879cc4f6ab5e94af36f752
hemat <hematullahw@gmail.com>
2024-05-09 12:57:19 +0430

    home page for docs

--- app.js
+++ app.js
@@ -8,13 +8,17 @@ const app = express();
 
 dbConnect();
 
+app.set("view engine", "ejs")
+app.use(express.static("public"))
 
 app.use(express.json());
+app.get("/", (req, res) => { res.render("home") })
 app.use("/api/contacts", contactRoutes);
 app.use("/api/users", userRoutes);
+app.use((req, res) => res.render("notfound"))
 app.use(require("./middlewares/errorHandler"));
 
 const port = process.env.PORT || 3000;
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server is runnning in port ${port}`);
 })
